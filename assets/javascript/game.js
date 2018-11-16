091d927f17a57f997e0190801c8c55b59b6f8e78
EFerri222 <eferri222@gmail.com>
2018-11-16 03:35:01 -0500

    added javascript to fix the method of winning; before it was letting you win if you guessed the last letter correctly even before guessing the others correctly, changed the name of splitAnswer to answerCharArray, fixed the issue of not being able to play again after the game was over by adding resets to the conditional statements that trigger if you win or lose, made it so correct answers would not be added to letters guessed, added some comments, added pokemon to answersArray to make it hold all original 150 pokemon

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,12 @@
-// var answersArray = ["bulbasaur", "charmander", "squirtle"];
-var answer = "bulbasaur";
+//Global Variables
+var win = false;
+var answersArray = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle" , "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran", "nidorina", "nidoqueen", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetch'd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mr. mime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"];
+
+var answer = answersArray[Math.floor(Math.random() * answersArray.length)];
+//Hold what the user has guessed
 var guessesArray = [];
-var splitAnswer = answer.split("");
+//holds an array of the characters of the answer
+var answerCharArray = answer.split("");
 
 var maskAnswer = function(str) {
     var res = [];
@@ -17,15 +22,18 @@ var guessedSoFar = maskAnswer(answer);
 // maskedAnswer.textContent = "test";
 // document.getElementById("answer-text").innerHTML = "Test";
 
+// Every time a user presses a key
 document.onkeyup = function(event) {
+    console.log(answerCharArray);
+    console.log(answersArray.length);
 
     // Determines which key was pressed.
     var userGuess = event.key;
+    var guessesCorrect = 0;
 
-    var guesses = document.getElementById("guesses-text");
     var guessesRemaining = document.getElementById("guesses-remaining-text");
-
     var repeat = false;
+    var correctAnswer = false;
 
     if (guessesArray.length === 0) {
         guessesArray.push(userGuess);
@@ -36,16 +44,23 @@ document.onkeyup = function(event) {
             }
         }
 
-        if (repeat === false) {
+        for (i=0; i<answerCharArray.length; i++) {
+            if (userGuess === answerCharArray[i]) {
+                correctAnswer = true;
+            }
+        }
+
+        if (repeat === false && correctAnswer === false) {
             guessesArray.push(userGuess);
         }
     }
 
+    var guesses = document.getElementById("guesses-text");
     // Adds guess to the guesses-remaining-text paragraph element.
-    guesses.textContent = guessesArray;
+    guesses.textContent = guessesArray.join(" ");
 
-    for (i=0; i<splitAnswer.length; i++) {
-        if (userGuess == splitAnswer[i]) {
+    for (i=0; i<answerCharArray.length; i++) {
+        if (userGuess === answerCharArray[i]) {
             guessedSoFar[i] = userGuess;
             document.getElementById("answer-text").innerHTML = guessedSoFar.join(" ");
         }
@@ -54,25 +69,36 @@ document.onkeyup = function(event) {
     // Updates the guesses remaining count
     guessesRemaining.textContent = 12 - guessesArray.length;
 
-    var win = false;
     var result = document.getElementById("result-text");
 
     for (i=0; i<guessedSoFar.length; i++) {
         if (guessedSoFar[i] !== "_ ") {
-            win = true;
-        } else {
-            win = false;
+            guessesCorrect++;
+
         }
+
+        if (guessesCorrect === answerCharArray.length) {
+            win = true; } else {
+                win = false;
+            }
     }
 
+    guessesCorrect = 0;
+
+    //You win
     if (win === true) {
         guessesRemaining.textContent = 12;
         guessesArray = [];
         guesses.textContent = "";
-        document.getElementById("answer-text").innerHTML = maskAnswer(answer).join(" ");
         result.textContent = "YOU WIN! :D Press any key to play again";
         document.getElementById("resultBox").style.display = "block";
+        answer = answersArray[Math.floor(Math.random() * answersArray.length)];
+        answerCharArray = answer.split("");
+        guessedSoFar = maskAnswer(answer);
+        document.getElementById("answer-text").textContent = maskAnswer(answer).join(" ");
     }
+
+    win = false;
 
     // If you run out of guesses, you lose
     if (guessesRemaining.textContent === "0") {
@@ -82,6 +108,10 @@ document.onkeyup = function(event) {
         // var result = document.getElementById("result-text");
         result.textContent = "You Lose! :( Press any key to play again";
         document.getElementById("resultBox").style.display = "block";
+        answer = answersArray[Math.floor(Math.random() * answersArray.length)];
+        answerCharArray = answer.split("");
+        guessedSoFar = maskAnswer(answer);
+        document.getElementById("answer-text").textContent = maskAnswer(answer).join(" ");
     }
 
     // Makes the result disappear once you press a key and the guesses remaining goes to 11
