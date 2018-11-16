b7663e6241d351a04e74f8902f4ebc0c19d6f45c
EFerri222 <eferri222@gmail.com>
2018-11-16 03:55:21 -0500

    changed the number of tries from 12 to 6 for increased difficulty, fixed a bug that would add the first letter guessed after a reset to letters guessed even if it were correct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,7 +36,14 @@ document.onkeyup = function(event) {
     var correctAnswer = false;
 
     if (guessesArray.length === 0) {
-        guessesArray.push(userGuess);
+        for (i=0; i<answerCharArray.length; i++) {
+            if (userGuess === answerCharArray[i]) {
+                correctAnswer = true;
+            }
+        }
+        if (correctAnswer === false) {
+            guessesArray.push(userGuess);
+        }
     } else {
         for (i=0; i<guessesArray.length; i++) {
             if (userGuess === guessesArray[i]) {
@@ -67,7 +74,7 @@ document.onkeyup = function(event) {
     }
 
     // Updates the guesses remaining count
-    guessesRemaining.textContent = 12 - guessesArray.length;
+    guessesRemaining.textContent = 6 - guessesArray.length;
 
     var result = document.getElementById("result-text");
 
@@ -87,7 +94,7 @@ document.onkeyup = function(event) {
 
     //You win
     if (win === true) {
-        guessesRemaining.textContent = 12;
+        guessesRemaining.textContent = 6;
         guessesArray = [];
         guesses.textContent = "";
         result.textContent = "YOU WIN! :D Press any key to play again";
@@ -102,7 +109,7 @@ document.onkeyup = function(event) {
 
     // If you run out of guesses, you lose
     if (guessesRemaining.textContent === "0") {
-        guessesRemaining.textContent = 12;
+        guessesRemaining.textContent = 6;
         guessesArray = [];
         guesses.textContent = "";
         // var result = document.getElementById("result-text");
@@ -115,7 +122,7 @@ document.onkeyup = function(event) {
     }
 
     // Makes the result disappear once you press a key and the guesses remaining goes to 11
-    if (guessesRemaining.textContent === "11") {
+    if (guessesRemaining.textContent === "5") {
         document.getElementById("resultBox").style.display = "none";
     }
   }
